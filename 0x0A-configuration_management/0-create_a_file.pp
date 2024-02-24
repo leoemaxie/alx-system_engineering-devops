723359c762515d5d5fbc0cb9b35378c988dffc67
leoemaxie <leoemaxie@gmail.com>
2024-02-24 01:57:37 +0100

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a tmp file school
+
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
